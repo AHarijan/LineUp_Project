3790886e52fd03e6fc53f1e683c8c731cb38c4b0
AHarijan <alakar_harijan@outlook.com>
2025-03-24 23:42:44 +0530

    push3

--- _Project/settings.py
+++ _Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['scornful-laure-vessel-lineup-64702854.koyeb.app']
 
 
 # Application definition
